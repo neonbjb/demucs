7d0f5c0e3fd0aa87cdb7e4f9ed53d49b99bbfb19
James Betker <jbetker@gmail.com>
2022-05-14 10:21:58 -0600

    Add better batching capabilities in inference
    Extends separate.py to:
    - Take in a path (containing .wav files) instead of a single file
    - Loads inputs using a torch dataloader in a separate process
    - Feeds model inputs as batches
    
    This significantly increases the processing speed for large numbers of files.

--- demucs/__main__.py
+++ demucs/__main__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .separate import main
+from .separate_multi import main
 
 if __name__ == '__main__':
     main()
